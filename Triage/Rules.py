373be9ccc23d2239dcc6dbc0c1180e1a2e1d939f
Raj102002 <114638801+Raj102002@users.noreply.github.com>
2026-02-21 22:44:53 +0530

    Modify risk function to include duration parameter
    Added duration parameter to risk function and updated logic.

--- Triage/Rules.py
+++ Triage/Rules.py
@@ -1,7 +1,12 @@
-def risk(mid,low,high):
+def risk(mid,low,high,duration):
   if high:
     return "high risk")
   if mid>7:
     return "medium"
+  if duration >=7 and low:
+    return "Consult a doctor"
   return low
-  
+
+#test
+x = risk(low = 3, high>=8)
+print(x)
